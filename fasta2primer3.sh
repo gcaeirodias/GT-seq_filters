a1b0d42a320f31652b2f00bd886f0a564375995d
Guilherme Caeiro-Dias <53879538+gcaeirodias@users.noreply.github.com>
2025-05-30 13:57:58 -0600

    Update fasta2primer3.sh

--- fasta2primer3.sh
+++ fasta2primer3.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 # Set directories and files
-DIR=/users/guidias/taos-scratch/code_tests/fasta_vcf
+DIR=[path to working directory]
 PRIMER3_FOLDER=primer3_input
-#GENOME=/users/guidias/taos-scratch/Bluntnose_shiner_genome/PBSragtag_scaffold_sort.fasta
-BED_GTSEQLOCI=/users/guidias/taos-scratch/code_tests/fasta_vcf/all_selected_GTseq_loci.bed
-BED_RADLOCI=/users/guidias/taos-scratch/code_tests/fasta_vcf/all_selected_RADloci.bed
-#FASTA_GTSEQLOCI=all_selected_GTseq_loci.fa
-FASTA_RADLOCI=/users/guidias/taos-scratch/code_tests/fasta_vcf/all_selected_RADloci.fa
+BED_GTSEQLOCI=all_selected_GTseq_loci.bed
+BED_RADLOCI=all_selected_RADloci.bed
+FASTA_RADLOCI=all_selected_RADloci.fa
+OUTPUT=[primer3 input].txt
 
-# Final output (input for primer3):
-OUTPUT=Nsim_2945loci_primer3_input_test.txt
-
-################## Main script ##################
 cd $DIR
 mkdir -p $PRIMER3_FOLDER
 cd $PRIMER3_FOLDER
-
-# Get fasta files
-#bedtools getfasta -fo $FASTA_GTSEQLOCI -fi $GENOME -bed $BED_GTSEQLOCI
-#bedtools getfasta -fo $FASTA_RADLOCI -fi $GENOME -bed $BED_RADLOCI
 
 # Identify target ranges to be sequenced by GT-seq to pass to Primer3 input
 awk 'NR==FNR {
