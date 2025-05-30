cf36000da1fd0bd9dbdf2437da591dfc0c123da6
Guilherme Caeiro-Dias <53879538+gcaeirodias@users.noreply.github.com>
2025-05-30 15:14:31 -0600

    Update GT-seq_loci_fasta_vcf.sh

--- GT-seq_loci_fasta_vcf.sh
+++ GT-seq_loci_fasta_vcf.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Set directories and files
-DIR="[path to working directory]"
-GENOME="[full path to FASTA genome file"
-RECOV_RADLOCI="[full path to BED file with recovered RAD loci]"
-RECOV_GTSEQLOCI="[full path to BED file with recovered GT-seq loci]"
-SELEC_RADLOCI="[full path to BED file with originaly selected RAD loci]"
-SELEC_GTSEQLOCI="[full path to BED file with originaly selected GT-seq loci]"
-VCF="[full path to original VCF file]"
+DIR=[path to working directory]
+GENOME=[genome].fasta
+RECOV_RADLOCI=recovered_RADloci.bed
+RECOV_GTSEQLOCI=recovered_GTseq_loci.bed
+SELEC_RADLOCI=selected_RADloci.bed
+SELEC_GTSEQLOCI=selected_GTseq_loci.bed
+VCF=[original].vcf
 
 # Set working directory
-cd "$DIR" || exit
+cd $DIR || exit
 
 # Merge BED files created by identify_GT-seq_loci.sh and rescue_loci.sh scripts and sort them
 merge_and_sort() {
@@ -18,14 +18,14 @@ merge_and_sort() {
     cat "$@" | sort -n -k1,1 -k2,2 -k3,3 | awk '{print $1,$2,$3}' | tr ' ' '\t' > "$output"
 }
 
-merge_and_sort "all_selected_GTseq_loci.bed" "$SELEC_GTSEQLOCI" "$RECOV_GTSEQLOCI"
-merge_and_sort "all_selected_RADloci.bed" "$SELEC_RADLOCI" "$RECOV_RADLOCI"
+merge_and_sort all_selected_GTseq_loci.bed $SELEC_GTSEQLOCI $RECOV_GTSEQLOCI
+merge_and_sort all_selected_RADloci.bed $SELEC_RADLOCI $RECOV_RADLOCI
 
 # Get FASTA sequences from genome FASTA file
 for file in "all_selected_RADloci" "all_selected_GTseq_loci"; do
-    bedtools getfasta -fo "${file}.fa" -fi "$GENOME" -bed "${file}.bed"
+    bedtools getfasta -fo "${file}.fa" -fi $GENOME -bed "${file}.bed"
 done
 
 # Get SNPs retained in the GT-seq loci from original VCF file
-sed -e '1i\chrom\tchromStart\tchromEnd' "all_selected_GTseq_loci.bed" > "all_selected_RADloci_header.bed"
-vcftools --vcf "$VCF" --bed "all_selected_RADloci_header.bed" --recode --recode-INFO-all --out "SNPs_all_selected_GT-seq_loci"
+sed -e '1i\chrom\tchromStart\tchromEnd' all_selected_GTseq_loci.bed > all_selected_RADloci_header.bed
+vcftools --vcf $VCF --bed all_selected_RADloci_header.bed --recode --recode-INFO-all --out SNPs_all_selected_GT-seq_loci
